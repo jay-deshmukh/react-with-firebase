81a64c17610f05c4b4ed775bd7b205a32c8faf70
Jay Deshmukh <35005482+jay-deshmukh@users.noreply.github.com>
2018-11-21 14:40:26 +0530

    minor fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 import firebase from 'firebase';
 const config = {
   apiKey:
-    'AIzaSyBI-PY00KPCnLXV-_MbMlATQdQeMfaGNHE',
+    '',
   authDomain:
-    'turtle-91b89.firebaseapp.com',
+    '',
   databaseURL:
-    'https://turtle-91b89.firebaseio.com',
+    '',
   projectId:
-    'turtle-91b89',
+    '',
   storageBucket:
-    'turtle-91b89.appspot.com',
+    '',
   messagingSenderId:
-    '252670856130',
+    '',
 };
 firebase.initializeApp(
   config
